96543394c868d8b132b8a6b0ab216dc70dc7a1bb
Shuin <garlen.javier@gmail.com>
2020-12-07 13:15:06 +0800

    - initial commit

---
+++ basic-mvvm/app/src/main/java/com/meekcode/mvvm/parceltracker/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.meekcode.mvvm.parceltracker.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.meekcode.mvvm.parceltracker.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
