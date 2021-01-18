908d6893b5a6f7d4cac718cce3756e866253096d
garlen-javier <71249192+garlen-javier@users.noreply.github.com>
2021-01-18 09:38:21 +0800

    Delete gatsby-passportjs directory

--- gatsby-passportjs/batch-script/netlify_lambda_install.bat
+++
@@ -1,6 +0,0 @@
-@setlocal enableextensions enabledelayedexpansion
-@echo off
-cd ..
-echo.Netlify Build Lambda
-npm run install:lambda
-pause
--- gatsby-passportjs/batch-script/netlify_lambda_start.bat
+++
@@ -1,6 +0,0 @@
-@setlocal enableextensions enabledelayedexpansion
-@echo off
-cd ..
-echo.Netlify Start Lambda
-npm run start:lambda
-pause
